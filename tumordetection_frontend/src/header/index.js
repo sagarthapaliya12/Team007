a0cc68db5174da927ed83d720712c967ab8f263e
Sagar Raj Thapaliya <68162024+sagarthapaliya12@users.noreply.github.com>
2022-08-13 16:06:55 +0545

    improved css of main page

--- tumordetection_frontend/src/header/index.js
+++ tumordetection_frontend/src/header/index.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
 
-
-
 export const MainWrapper = styled.div`
+position: absolute;
 height: 100vh;
-width: 100%;
+width: 100vw;
+overflow: hidden;
 background: #ffffff;
-padding: 1rem;
+/* padding: 1rem; */
 
 `
 export const Header = styled.div`
-width: 200%;
 font-size: 3rem;
 font-weight: 450;
 color: #ffffff;
 margin-left:30px;
+padding: 10px;
 
 `
